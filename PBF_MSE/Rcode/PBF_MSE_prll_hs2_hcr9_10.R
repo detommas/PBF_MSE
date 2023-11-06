9b28b3f8825270af89b0217a894e3d3f55c2ffaa
detommas <62910036+detommas@users.noreply.github.com>
2023-11-06 13:46:23 -0800

    Add files via upload

---
+++ PBF_MSE/Rcode/PBF_MSE_prll_hs2_hcr9_10.R
@@ -0,0 +1,83 @@
+#North Pacific Bluefin tuna MSE code 
+
+#this runs the PBF MSE framework for 21 years for the specified iteration
+#iterations differ in their recruitment deviations and random implementation errors
+#with an assessment every three years
+
+#clean up the workspace
+rm(list=ls())
+
+#source all the packages needed
+library(foreach)
+library(doParallel)
+library(r4ss)
+library(Rcpp)
+library(dplyr)
+library(reshape2)
+
+#Specify path of parent directory
+pdir = "J:/Desiree/Bluefin/PBF_MSE/"
+#set working directory to where all functions needed are stored
+setwd(paste(pdir,"Rcode/R_funs", sep = ""))
+
+#Specify the path of conditioned initial OM
+sdir = "J:/Desiree/Bluefin/PBF_MSE/Condition/"
+
+#source all the functions
+file.sources = list.files()
+sapply(file.sources,source,.GlobalEnv)
+
+#specify the frequency of assessments in years
+tasmt = 3
+
+#we are projecting 24 years into the future
+#we assume the assessment occurs every 3 years, so there are 10 assessment time steps 
+#below we specify when those happen in the 24 years time series as first management action really occurs three years into the simulation
+asmt_t = seq(1, 24, by=tasmt)
+
+#import needed files
+#catch ratios by fleet and season for each of the fleet groups obtained by running the bluefin_om_base.R code 
+cr_wl= read.csv(paste(pdir,"crwl.csv", sep = ""))
+cr_ws= read.csv(paste(pdir,"crws.csv", sep = ""))
+cr_wm= read.csv(paste(pdir,"crwm.csv", sep = ""))
+cr_epo= read.csv(paste(pdir,"crepo.csv", sep = ""))
+cr_all = read.csv(paste(pdir,"crdat.csv", sep = ""))
+
+#Set the harvest strategy
+hsnum = 2
+#Set the HCR
+hcrnum=9
+#Set the scenario
+scnnum=1
+
+#Calculate the numbers of cores 
+no_cores = detectCores() - 1
+
+#Initiate cluster
+cl= makeCluster(no_cores)
+registerDoParallel(cl)
+
+#Run the MSE code for specified number of iterations
+#Bthr is the SSB based biomass reference point. It represents the fraction of unfished SSB
+#Blim is the limit reference point represting the fraction of unfished SSB
+#Note that the F based TRP is specified already in the forecast file 
+#the output is already saved as the code runs in the respective folders
+#main output to then compute performance metrics is the outlist.text file created for each iteration
+foreach(itr = 1:100, .packages = c('r4ss','dplyr','reshape2')) %dopar% { PBF_MSE_hs2_910_for(hsnum,hcrnum,scnnum,itr, Bthr = 0.20,sa=0)}
+
+#terminate cluster
+stopCluster(cl)
+
+#Initiate cluster
+cl= makeCluster(no_cores)
+registerDoParallel(cl)
+
+#Set the harvest strategy
+hsnum = 2
+#Set the HCR
+hcrnum=10
+#Set the scenario
+scnnum=1
+foreach(itr = 1:100, .packages = c('r4ss','dplyr','reshape2')) %dopar% { PBF_MSE_hs2_910_for(hsnum,hcrnum,scnnum,itr, Bthr=0.15, sa=0)}
+
+stopCluster(cl)
---
+++ PBF_MSE/Rcode/read_outpt_pbfMSE.R
@@ -0,0 +1,64 @@
+#Combine outputs from all iterations in one dataframe
+
+library(r4ss)
+
+#specify the HCR that is being run
+hsnum = 1
+hs = paste(hsnum, "/", sep = "")
+
+#specify the HCRs that were run
+hcrnumv = c(108)
+
+#Specify the scenario being run
+snum = 1
+scn = paste(snum, "/", sep ="")
+
+#Specify parent directories path 
+pdir= "J:/Desiree/Bluefin/PBF_MSE/"
+#pdir = "C:/Users/desiree.tommasi/Documents/Bluefin/PBF_MSE/"
+#pdir = "C:/Users/FRDScientist/Documents/Desiree/MSE_2/MSE_ALB/"
+
+for (j in 1:length(hcrnumv)){
+  
+  #specify the hcr to extract
+  hcrnum = hcrnumv[j]
+  hcr = paste(hcrnum, "/", sep = "")
+  
+  #Extract results from each iteration and add to the same matrix
+  itrn =NA
+  
+  for (itr in 1:100){
+    #check what is in the working directory
+    itr_d= paste(pdir, hs, hcr, scn, itr, sep = "")
+    
+    itr_f = list.files(itr_d)
+    
+    #only extract data if EM completed
+    
+    if ("outlist.txt" %in% itr_f == TRUE){
+      itrn = c(itrn,itr)
+    }
+  }
+  
+  itrn = itrn[-1]
+  ind = seq(1,(length(itrn)*24), by = 24)
+  smat = matrix(NaN, nrow = (length(itrn)*24), ncol = 48)
+  smat = as.data.frame(smat)
+  
+  
+  for (itr in 1:length(itrn)) {
+    for (c in 1:47){
+      #import simulation data fo reach iteration
+      dat= read.table(paste(pdir,hs, hcr, scn, itrn[itr],"/outlist.txt", sep =""))
+      smat[ind[itr]:(ind[itr]+23), ] = dat
+    }
+    #add a column to specify the iteration
+    smat[ind[itr]:(ind[itr]+23), 48] = rep(itrn[itr], 24)
+  }
+  
+  names(smat) = c(names(dat),"itr")
+  
+  #save output to file
+  write.table(smat, paste(pdir,hs,hcr, scn, "/outmat",hsnum,hcrnum,snum,".txt", sep =""))
+  
+}
