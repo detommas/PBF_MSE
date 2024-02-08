c09545345ec495e2e13482fb0373a82081ecd2f8
detommas <62910036+detommas@users.noreply.github.com>
2024-02-08 13:07:44 -0800

    edited explanatory text

--- PBF_MSE/Rcode/R_funs/recdevs_mse.R
+++ PBF_MSE/Rcode/R_funs/recdevs_mse.R
@@ -2,8 +2,7 @@
 #' This function is based on the ss3sim get_recdevs.r by Kelli Johnson
 #' This function returns a set of recruitment deviations based
 #' on an iteration number and a sigma r. Given the same iteration number the function will
-#' return the same recruitment deviations, given the sigma r from th 2017 albacore stock assessment, 0.5.
-#' Note that the  
+#' return the same recruitment deviations
 #' @param iteration The iteration number. This is used as an ID to set the
 #'   random number seed.
 #' @param n The length of the vector returned.
@@ -11,11 +10,12 @@
 #' @param seed An integer value to pass to \code{\link[base]{set.seed}}
 #' @return A vector of recruitment deviations.
 
-recdevs_mse <- function(iteration, n, sigma, rho, seed = 21) {
+recdevs_mse <- function(iteration, n, sigma, seed = 21) {
   set.seed(seed)
   x = sample(1:1e6)[iteration]
   set.seed(x)
   rec_devs = rnorm(n, 0, sigma)
-   
+  #ensure they have mean 0
+  #rec_devs2 = rec_devs-mean(rec_devs)
   return(rec_devs)
 }
