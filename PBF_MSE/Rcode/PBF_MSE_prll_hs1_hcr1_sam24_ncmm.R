ff0e8a8555fb396565e9ac44d243d3bb701f767a
detommas <62910036+detommas@users.noreply.github.com>
2024-09-09 11:33:43 -0700

    added new explanatory comments

--- PBF_MSE/Rcode/PBF_MSE_prll_hs1_hcr1_sam24_ncmm.R
+++ PBF_MSE/Rcode/PBF_MSE_prll_hs1_hcr1_sam24_ncmm.R
@@ -65,9 +65,16 @@ registerDoParallel(cl)
 #Run the MSE code for specified number of iterations
 #Bthr is the SSB based biomass reference point. It represents the fraction of unfished SSB
 #Blim is the limit reference point representing the fraction of unfished SSB
+#sa specifies if a stock assessment shoudl be run (1) or not (0)
+#Fmin specific the fraction of the fishing intensity at the target reference point that correpsonds to the minimum fishign intensity
+#lag specifies the lag between data availbility and the assessment time step
+#obse specifies if to use the expected values (2) or bootstrap (3) from the bootstrap data file that is fed into the EM
+#aspm specifies if, when sa=1, to use the aspm EM (aspm ="ASPMR-f1f12") or the full EM (aspm=NULL). 
+#yfor specifies the years the selectivity and relF are averaged over to find benchamark quantities and for the forecast
+#tacl specifies if the 25% change on TAC change between assessment periods should be apploed (tacl=25) or not (tacl=0)
 #Note that the F based TRP is specified already in the forecast file 
 #the output is already saved as the code runs in the respective folders
-#main output to then compute performance metrics is the outlist.text file created for each iteration
+#main output to then compute performance metrics is the outlist.text file
 foreach(itr = 1:100, .packages = c('r4ss','dplyr','reshape2')) %dopar% { PBF_MSE_hs1_for_sam24_ncmm(hsnum,hcrnum,scnnum,itr, Bthr = 0.20, Blim = 0.15,sa=1,Fmin=0.10,lag=1,obse=3,aspm=NULL,yfor=c(2015:2022),tacl=25)}
 
 #terminate cluster
