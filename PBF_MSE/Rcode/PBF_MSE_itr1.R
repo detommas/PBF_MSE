a2e31ffb6cdc4e81ea36e67ef2c6761993bf576f
detommas <62910036+detommas@users.noreply.github.com>
2024-09-09 11:31:31 -0700

    Update PBF_MSE_itr1.R
    updated to use new OM

--- PBF_MSE/Rcode/PBF_MSE_itr1.R
+++ PBF_MSE/Rcode/PBF_MSE_itr1.R
@@ -1,6 +1,6 @@
 #North Pacific Bluefin tuna MSE code to run one iteration
 
-#this runs the PBF MSE framework for 24 years for the HCR and iterations
+#this runs the PBF MSE framework for 21 years for the HCR and iterations
 #iterations differ in their recruitment deviations and random implementation errors
 
 #clean up the workspace
@@ -13,6 +13,7 @@ library(r4ss)
 library(Rcpp)
 library(dplyr)
 library(reshape2)
+library(tidyverse)
 
 #Specify path of parent directory
 pdir = "C:/Users/desiree.tommasi/Documents/Bluefin/PBF_MSE/"
@@ -32,15 +33,17 @@ tasmt = 3
 #we are projecting 24 years into the future
 #we assume the assessment occurs every 3 years, so there are 10 assessment time steps 
 #below we specify when those happen in the 24 years time series as first management action really occurs three years into the simulation
-asmt_t = seq(1, 24, by=tasmt)
+asmt_t = seq(2, 23, by=tasmt)
 
 #import needed files
 #catch ratios by fleet and season for each of the fleet groups obtained by running the bluefin_om_base.R code 
-cr_wl= read.csv(paste(pdir,"crwl.csv", sep = ""))
-cr_ws= read.csv(paste(pdir,"crws.csv", sep = ""))
-cr_wm= read.csv(paste(pdir,"crwm.csv", sep = ""))
-cr_epo= read.csv(paste(pdir,"crepo.csv", sep = ""))
-cr_all = read.csv(paste(pdir,"crdat.csv", sep = ""))
+ciom= read.csv(paste(pdir,"ciom.csv", sep = ""))
+ciom25= read.csv(paste(pdir,"ciom25.csv", sep = ""))
+
+#read selectivity deviations
+#sdev= read.csv(paste(pdir,"sdev1720.csv", sep = ""))
+#sdev= read.csv(paste(pdir,"sdev1722.csv", sep = ""))
+sdev= read.csv(paste(pdir,"sdev1522.csv", sep = ""))
 
 #Set the harvest strategy
 hsnum = 1
@@ -60,10 +63,12 @@ itr=1
 #Fmin specific the fraction of the fishing intensity at the target reference point that correpsonds to the minimum fishign intensity
 #lag specifies the lag between data availbility and the assessment time step
 #obse specifies if to use the expected values (2) or bootstrap (3) from the bootstrap data file that is fed into the EM
-#aspm specifies if, when sa=1, to use the aspm EM (aspm ="ASPMR-f1f12") or the full EM (aspm=NULL). This aspm is ASPM-R with size & selectivities for F1 & F12
+#aspm specifies if, when sa=1, to use the aspm EM (aspm ="ASPMR-f1f12") or the full EM (aspm=NULL). 
+#yfor specifies the years the selectivity and relF are averaged over to find benchamark quantities and for the forecast
+#tacl specifies if the 25% change on TAC change between assessment periods should be apploed (tacl=25) or not (tacl=0)
 #Note that the F based TRP is specified already in the forecast file 
 #the output is already saved as the code runs in the respective folders
 #main output to then compute performance metrics is the outlist.text file
-#Note tha tthe reference points specified below are for HCR1. There is no HCR15 in the latest set of candidate HCRs
+#Note tha the reference points specified below are for HCR1. There is no HCR15 in the latest set of candidate HCRs
 
-PBF_MSE_hs1_for(hsnum,hcrnum,scnnum,itr, Bthr = 0.2, Blim = 0.15,sa=0,Fmin=0.10, lag=0, obse=2,aspm=NULL)
+PBF_MSE_hs1_for_sam24_ncmm(hsnum,hcrnum,scnnum,itr, Bthr = 0.20, Blim = 0.15,sa=1,Fmin=0.10,lag=1,obse=3,aspm=NULL,yfor=c(2015:2022),tacl=25)
